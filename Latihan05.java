8b179357afa1d4eec5d67de1ddbac807241c0dc3
Fatmaazzahra24 <azzahrafatma727@gmail.com>
2024-09-06 09:28:04 +0700

    commit kedua saya

---
+++ Latihan05.java
@@ -0,0 +1,29 @@
+public class Latihan05 {
+    public static void main(String[] args) {
+       
+        System.out.print("Nama Saya Fatma azzahra Alif Hidayah ");
+        System.out.print("Saya adalah mahasiswa baru Jurusan teknologi Informasi-POLINEMA ");
+
+        
+        System.out.println("----------------------------------------------");
+        System.out.println("--Data Mahasiswa Jurusan Teknologi Informasi--");
+        System.out.println("----------------------------------------------");
+        System.out.println("Nama     : Fatma azzahra Alif Hidayah ");
+        System.out.println("NIM      : 244107060046");
+        System.out.println("Kelas    : 1A ");
+        System.out.println("No.Absen : 05 ");
+        System.out.println("Prodi    : Sistem Informasi Bisnis ");
+        System.out.println("Alamat   : Jl.Semanggi Barat ");
+        System.out.println("No HP    : 0881026863305 ");
+        System.out.println("Hobi     : memasak"); 
+        
+        }
+
+    }
+
+       
+
+
+
+
+    
---
+++ MyFirstJava05.java
@@ -0,0 +1,5 @@
+public class MyFirstJava05 {
+    public static void main(String[] args) {
+        System.out.println("fatma azzahra alif hidayah");
+    }
+}
